26292c79d2f51abda1128dc695be67ce659f8997
SulRinM <108032806+SulRinM@users.noreply.github.com>
2023-01-31 19:01:36 +0300

    remove LEAVERLIBRARYDYNAMIC_EXPORTS

--- Leaver.h
+++ Leaver.h
@@ -2,13 +2,8 @@
 #include <iostream>
 #include <string>
 
-#ifdef LEAVERLIBRARYDYNAMIC_EXPORTS
-#define LEAVERLIBRARY_API __declspec(dllexport)
-#else 
-#define LEAVERLIBRARY_API __declspec(dllimport)
-#endif
 class Leaver
 {
 public:
-	LEAVERLIBRARY_API std::string leave(std::string name);
+	 std::string leave(std::string name);
 };
