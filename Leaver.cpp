ed45e617a20f66a7225955db364d8f0ed2968231
SulRinM <108032806+SulRinM@users.noreply.github.com>
2023-01-31 14:02:50 +0300

    Добавьте файлы проекта.

---
+++ Leaver.cpp
@@ -0,0 +1,6 @@
+#include "Leaver.h"
+
+std::string Leaver::leave(std::string name)
+{
+	return "Goodbye, " + name + "!";
+}
---
+++ Leaver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#ifdef LEAVERLIBRARYDYNAMIC_EXPORTS
+#define LEAVERLIBRARY_API __declspec(dllexport)
+#else 
+#define LEAVERLIBRARY_API __declspec(dllimport)
+#endif
+class Leaver
+{
+public:
+	LEAVERLIBRARY_API std::string leave(std::string name);
+};
